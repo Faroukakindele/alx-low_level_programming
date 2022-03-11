10d11129124969c11a1343b66387cb1e616e5d09
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-11 21:11:33 +0100

    j

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main-Program entry point.
+ *
+ *Return: 0-no error, non-zero value if errors.
+ */
+int main(void)
+{
+	 char i;
+	
+	 for(i = '0'; i < '10'; i++)
+	 {
+		putchar(i);
+	 }
+	
+	 
+	putchar('\n');	
+	return (0);
+}
