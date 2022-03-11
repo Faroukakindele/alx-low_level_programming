a14ce8a5823fcbdc090fea8ac52fdaf2770fbc10
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-11 21:05:52 +0100

    nn

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main-Program entry point.
+ *
+ *Return: 0-no error, non-zero value if errors.
+ */
+int main(void)
+{
+	char i;
+	 for(i = 'a'; i <= 'z'; i++)
+	 {
+		if (i == 'e' || i == 'q')
+		{
+			continue;
+		}
+		 
+		putchar(i);
+	 }
+	putchar('\n');	
+	return (0);
+}
