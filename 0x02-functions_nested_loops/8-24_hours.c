a6db4353e99615f67fe6fa832942ac47e28c37ff
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-15 23:37:53 +0100

    Create 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_last_digit - get the last value value of the number from zero.
+* @r: character to test.
+* Return:the absolute value
+*/
+
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
+
+
+
