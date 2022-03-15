f47e98e4e2fd28e81570f6ea0c0eced1702d40b2
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-15 23:08:21 +0100

    nn

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* int _abs - tests the absolute sign  of a character
+* @r: character to test.
+* Return:the absolute value
+*/
+
+int _abs(int r)
+{
+	if(r >= 0)
+	{
+		return(r);
+	}
+	else
+	{
+		return(r *-1);
+	}
+}
+
+
+
