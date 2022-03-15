dab090714d716b0b628fe17a878cd8de9a651903
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-15 23:23:30 +0100

    bb

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _abs - calculate the absolute value of the number from zero.
+* @r: character to test.
+* Return:the absolute value
+*/
+
+int print_last_digit(int r)
+{
+	int n;
+	if (r >= 0)
+	{
+		 n = (-1 * r) % 10;
+	}
+	else
+	{
+	  n = r % 10;
+	}
+	return (n)
+}
+
+
+
