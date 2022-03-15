8b0c14792adc5b04f474968300c044273b8563fa
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-15 21:54:34 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * _islower - tests whether a character is a
+  * _isalpha - tests whether a character is a
  * lowercase  or uppercase or a number letter from the English alphabet.
  * @c: character to test.
  * Return: 1 if it is a obeys the condition, 0 if it disobeys
