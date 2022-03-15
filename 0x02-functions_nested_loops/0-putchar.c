96e6b833520794957aec1c6ad0171f4d0e1aa37f
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-15 20:42:12 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,14 +4,14 @@
  *main-program entry point for execution.
  *Return:0-success
  */
-int main (void)
+int main(void)
 {
 		char word[9] = "_putchar\n";
 		int i;
 
-		for ( i = 0; i < 9; i++)
+		for (i = 0; i < 9; i++)
 		{
 			_putchar(word[i]);
 		}
-		return 0;
+		return (0);
 }
