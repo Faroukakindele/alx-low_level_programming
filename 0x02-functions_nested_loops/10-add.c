49849f3eb7baaff5259dd36cfe811f0c173df48e
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-16 00:10:20 +0100

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * Description: It prints 9 times table starting with 0
+ * Return: Nothing.
+ */
+
+int add(int a, int b)
+{
+	int n;
+	n = a + b;
+	return(n);
+}
+
+
