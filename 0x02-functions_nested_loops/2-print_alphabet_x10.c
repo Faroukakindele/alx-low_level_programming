0b0327400de32e89e22e1abaef6d4783b87ecb13
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-15 21:11:52 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,19 +9,16 @@ void print_alphabet_x10(void)
 {
 	int i;
 	char c;
-	for ( i = 0; i <=10; i++)
+	for ( i = 0; i<10; i++)
 	{
-		char c = 'a';
-		while (c <= 'z')
+		for (c = 'a'; c < 'z'; c++)
 		{
 			_putchar(c);
-			c++;
 		}
+		
 		_putchar('\n');
-			i++;
-	}
 	
- 
+	}
 	
 }
 
