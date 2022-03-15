bd84a061dffa66c4ba68ed476d8969bd21de9c3d
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-15 22:34:10 +0100

    nnew

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * print_sign - teststhe sign  of a character
+ * @c: character to test.
+ * Return: 1 if it is positive and 0 if its negative and  -1 if obeys the condition, 0 if it disobeys
+ */
+int print_sign(int n)
+{
+	if ( n < 0)
+	{
+		_putchar("-");
+		return (-1);
+	}
+	else
+	if( n > 0) 
+	{
+		_putchar("+");
+		return (1);
+	}
+	else
+	if(n == 0)
+	{
+		_putchar('0');
+		return(0);
+	}
+}
+
+
+
