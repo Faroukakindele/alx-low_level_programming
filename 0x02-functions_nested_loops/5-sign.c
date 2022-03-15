eb0efd1398fb00984f8a7ae6649f419ea124d488
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-15 22:53:18 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,16 +5,15 @@
  * @n: character to test.
  * Return: 1 if it is positive and 0 if its negative and 
  *  -1 if obeys the condition, 0 if it disobey
- * 
  */
 int print_sign(int n)
 {
-	if ( n < 0)
+	if (n < 0)
 	{
 		_putchar('-');
-		return(-1);
+		return (-1);
 	}
-	else if ( n > 0) 
+	else if (n > 0) 
 	{
 		_putchar('+');
 		return (1);
