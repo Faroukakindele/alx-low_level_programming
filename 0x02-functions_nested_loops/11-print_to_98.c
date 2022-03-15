d4f2550193eee047afc20297c90223b9e7828fb3
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-16 00:41:34 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,17 +12,17 @@ void print_to_98(int n)
 	{
 		for (n = n; n <= 98; n++)
 		{
-			printf("%d",n);
+			printf("%d ," ,n);
 		}
-		  printf("%d",98);
+		  printf("%d\n",98);
 	}
 	if(n> 98)
 	{
 		for ( n = n; n > 98; n--)
 		{
-			printf("%d",n);
+			printf("%d ,",n);
 		}
-		printf("%d",98);
+		printf("%d\n",98);
 	}
 }
 
