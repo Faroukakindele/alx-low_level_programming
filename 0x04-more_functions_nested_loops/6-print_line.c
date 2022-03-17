a436acc136b1e12951b56e099a35dbac6ca1b1fe
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-18 00:26:47 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_line - prints a character in a straight line
- * @n character to test 
- * Return: nothing
+ * print_line - draw a straight line
+ * @n: number of times underscore is printed
+ * Description: Can only use _putchar to print
  */
 
 void print_line(int n)
