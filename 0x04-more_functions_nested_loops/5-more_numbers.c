ac3dd1362b59f763f1ad54017633ea979c92e81e
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-17 23:45:28 +0100

    cc

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *more_numbers - prints the numbers, from 0 to 14, i4 times followed by a new line
+ *more_numbers- prints the numbers, from 0 to 14, i4 times followed by a new line
  * Return: nothing
  */
 
