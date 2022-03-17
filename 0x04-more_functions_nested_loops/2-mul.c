29b778eae4ed5aa55f7fea42a716c55dc5ec20a7
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-17 15:58:20 +0100

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_isupper - a function that checks for numberdigits
+ *@a: a variable that stores letters
+ *@b: b variable that stores letters
+ *Return: Returns a value 
+ *letter.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
