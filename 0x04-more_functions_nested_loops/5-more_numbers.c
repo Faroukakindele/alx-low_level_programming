42d5f56a2cc98fcfa7801cfba45d84ba9d09a5f4
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-17 23:34:33 +0100

    b

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,20 +4,23 @@
  * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line but not 2 or 4
  * Return: nothing
  */
-
 void more_numbers(void)
 {
-	int i;
+	int c, i;
 
-	for (i = 0; i < 10; i++)
+	c = 0;
+	while (c < 10)
 	{
-		for (i = 0; i <= 14; i++)
+		i = 0;
+		while (i < 15)
 		{
-			_putchar(i);
+			if (i > 9)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
+			i++;
 		}
-		
+		c++;
 		_putchar('\n');
 	}
-	_putchar('\n');
-
 }
