e45ec1c307f8df7410339cc7f09bf8a71f094f13
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-17 11:17:27 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
-* print_sign - teststhe sign  of a character
+* postitive_or_negativee - teststhe sign  of a character
 * @n: character to test.
 * Return:1 if it is positive and 0 if its negative and 
 * -1 if obeys the condition, 0 if it disobey
 */
 
-int print_sign(int n)
+int positive_or_negative(int n)
 {
 	if (n < 0)
 	{
