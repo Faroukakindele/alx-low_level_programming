a2c24398b8b4276c8c88771d11b01921cf642c4c
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-17 15:40:47 +0100

    Create 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_isupper - a function that checks for numberdigits
+ *@c: a variable that stores letters
+ *Return: Returns a value of 1 if the variable c contains an uppercase
+ *letter.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
