58087b9469e7f6f06515e9c3c45d031bfb115a10
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-18 00:10:13 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,21 +7,19 @@
 
 void more_numbers(void)
 {
-	int c, i;
+	int i;
+	int c;
 
-	c = 0;
-	while (c < 10)
+	for (i = 0; i < 10; i++)
 	{
-		i = 0;
-		while (i < 15)
+		for (c = 0; c <= 14; c++)
 		{
-			if (i > 9)
-				_putchar(i / 10 + '0');
-
-			_putchar(i % 10 + '0');
-			i++;
+			putchar(c);
 		}
-		c++;
-		_putchar('\n');
+
+		putchar('\n');
 	}
+	putchar('\n');
+
 }
+
