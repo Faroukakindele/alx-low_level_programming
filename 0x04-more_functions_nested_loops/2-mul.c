a0771a141dfdcbebbf8d93792a7fa6f2a928ba83
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-17 16:03:24 +0100

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_isupper - a function that checks for numberdigits
+ *mul - a function that checks for numberdigits
  *@a: a variable that stores letters
  *@b: b variable that stores letters
  *Return: Returns a value 
