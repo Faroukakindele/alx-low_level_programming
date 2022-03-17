8af25932cb617b47d14e5c94d4b1708e24684d64
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-17 12:03:30 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,13 @@
-#include "main.h"
-
 /**
 * main - tests function that prints if integer is positive or negative
-* @ - character to test
-*return: 0
-*
+* Return: 0
 */
 
 int main(void)
 {
         int i;
 
-        i = 0;
+        i = 98;
         positive_or_negative(i);
 
         return (0);
