1996dcf9ce4bd4f9af100697d1b56ff76ca6eea5
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-17 22:48:51 +0100

    new

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - prints a character in a straight line
+ * Return: nothing
+ */
+
+void print_line(int n)
+{
+  for ( n = n; n < n +1; n++)
+  {
+	  _putchar('_');
+  }
+  
+
+}
