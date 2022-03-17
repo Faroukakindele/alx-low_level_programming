37e3f7cffbba7786ce97d3991ff7127b31f2efc1
Faroukakindele <97978162+Faroukakindele@users.noreply.github.com>
2022-03-17 18:25:57 +0100

    bb

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line but not 2 or 4
+ * Return: nothing
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (i = 48; i < 58; i++)
+		{
+			_putchar(i);
+		}
+		
+		_putchar('\n');
+	}
+	_putchar('\n');
+
+}
